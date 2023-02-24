f4f6206a86f1a40fd9dbcda8a6297d85b3a420fe
zhenyu wu <33506993+wzy6642@users.noreply.github.com>
2023-02-24 09:47:37 +0800

    Add files via upload

--- code/draw_map.py
+++ code/draw_map.py
@@ -1,33 +1,33 @@
-# 本项目旨在通过获取"GPS记录器app"的定位数据绘制用户的路线图
-# 首先需要将GPS记录器的定位数据上传到data文件夹下，文件格式为geojson
+# 本项目旨在通过获取"两步路"的定位数据绘制用户的路线图
+# 首先需要将GPS记录器的定位数据上传到data文件夹下，文件格式为kml
 # 全局科学上网能加速加载线路图
 # pip install folium
-import json
-import datetime
-
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
 import folium
 from folium import PolyLine
 
-## 初始化配置
-data_path = r'../data\nse-2720013373226014400-20230223.geojson.geojson' # 原始数据的存储路径
-save_path = r'../map/20230223.html'                                     # 生成地图的存储路径
-time_format = "%Y-%m-%d %H:%M:%S"                                       # 时间格式
-_3D = False                                                             # 如果为True则返回3维卫星地图，否则为2维地图
 
-## 加载原始数据
-with open(data_path, 'r') as f:
-    data = json.load(f).get('features')
+data_path = r'../data\nse-5442193384823581830-2023-02-23 2119西安长安区.kml.kml'
+save_path = r'../map/20230224.html'                                             # 生成地图的存储路径
+_3D = True                                                                     # 如果为True则返回3维卫星地图，否则为2维地图
 
-def get_gps_info(sub_data):
-    timestamp = datetime.datetime.fromisoformat(sub_data.get('properties').get('time')).strftime(time_format)
-    geometry = sub_data.get('geometry').get('coordinates')
-    longitude, latitude = geometry
-    return [timestamp, latitude, longitude]
+with open(data_path, 'r', encoding='utf-8') as f:
+    soup = BeautifulSoup(f, 'xml')
+    track = soup.find('gx:Track')
+    content = str(track)
 
-gps_info = list(map(get_gps_info, data))
+pattern = re.compile(r'<gx:coord>(.*?)</gx:coord>', re.S)
+locations = pattern.findall(content)
+locations = [location.strip().split() for location in locations]    # 经度、纬度、海拔
+locations = [list(map(float, location)) for location in locations]
+pattern = re.compile(r'<when>(.*?)</when>', re.S)
+timestamps = pattern.findall(content)
+timestamps = [datetime.strptime(timestamp.strip(), '%Y-%m-%dT%H:%M:%SZ').strftime('%Y-%m-%d %H:%M:%S') for timestamp in timestamps]
 
 ## 地图初始化
-map = folium.Map(location=gps_info[0][-2:], zoom_start=15)
+map = folium.Map(location=locations[0][:2][::-1], zoom_start=15)
 if _3D:
     folium.TileLayer(
         tiles="https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga",
@@ -38,13 +38,15 @@ if _3D:
     ).add_to(map)
 
 ## 在地图上添加点以及绘制线路图
-for sub_data in gps_info:
-    marker = folium.Marker(location=[sub_data[1], sub_data[2]], tooltip=sub_data[0], popup=f'<div style="width:150px;height:45px;">时间: {sub_data[0]}<br>纬度: {sub_data[1]}<br>经度: {sub_data[2]}</div>')
+for idx in range(0, len(locations), 3):
+    location = locations[idx]
+    timestamp = timestamps[idx]
+    marker = folium.Marker(location=[location[1], location[0]], tooltip=timestamp, popup=f'<div style="width:150px;height:55px;">时间: {timestamp}<br>纬度: {location[1]}<br>经度: {location[0]}<br>海拔: {location[2]}</div>')
     marker.add_to(map)
 
 route = PolyLine(
-    locations=[sub_data[-2:] for sub_data in gps_info],
-    text="行车路线",
+    locations=[sub_data[:2][::-1] for sub_data in locations],
+    text="路线",
     offset=10,
     color="blue",
     weight=5,
@@ -53,3 +55,9 @@ route = PolyLine(
 )
 route.add_to(map)
 map.save(save_path)
+
+# 1、浏览历史线路图，并按照时间进行筛选（连接数据库）
+# 2、将照片添加到地图上
+# 3、对比pyechars与folium两种实现方式的不同，寻找一种最佳实现方式
+# 4、利用PyQt5构建一个GUI界面
+# 5、如果要添加图片要考虑有效地压缩图片
